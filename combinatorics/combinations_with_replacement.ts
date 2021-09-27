af016a370c26e886f92be001a298981f6f6043e6
Nathan Brooks <natebrooks004@gmail.com>
2021-09-27 08:21:06 -0700

    combinations_with_replacement

---
+++ combinatorics/combinations_with_replacement.ts
@@ -0,0 +1,29 @@
+/** Yields `r` length subsequences of elements from the input `iterable` allowing with repetition. */
+export function* combinationsWithReplacement<T>(
+  iterable: Iterable<T>,
+  r: number,
+): Generator<T[]> {
+  if (r < 0) {
+    throw RangeError("r must be non-negative");
+  }
+  const pool = [...iterable];
+  const n = pool.length;
+  if (n === 0 && r > 0) {
+    return;
+  }
+  const indices = Array(r).fill(0);
+  yield indices.map((i) => pool[i]);
+  while (true) {
+    let i = r - 1;
+    any: {
+      for (; i >= 0; i--) {
+        if (indices[i] !== n - 1) {
+          break any;
+        }
+      }
+      return;
+    }
+    indices.fill(indices[i] + 1, i);
+    yield indices.map((i) => pool[i]);
+  }
+}
---
+++ test/combinations_with_replacement_test.ts
@@ -0,0 +1,82 @@
+import {
+  assertEquals,
+  assertThrows,
+} from "https://deno.land/std@0.108.0/testing/asserts.ts";
+import { combinationsWithReplacement } from "../combinatorics/combinations_with_replacement.ts";
+
+Deno.test("negative r", () => {
+  assertThrows(
+    () => [...combinationsWithReplacement("abc", -1)],
+    RangeError,
+    "r must be non-negative",
+  );
+});
+
+Deno.test("r = 0", () => {
+  const expected = [[]];
+  const actual = [...combinationsWithReplacement("abc", 0)];
+  assertEquals(actual, expected);
+});
+
+Deno.test("r = n = 0", () => {
+  const expected = [[]];
+  const actual = [...combinationsWithReplacement("", 0)];
+  assertEquals(actual, expected);
+});
+
+Deno.test("n = 0, r > 0", () => {
+  const actual = [...combinationsWithReplacement("", 1)];
+  assertEquals(actual, []);
+});
+
+Deno.test("r > n", () => {
+  const expected = [
+    ["a", "a", "a", "a"],
+    ["a", "a", "a", "b"],
+    ["a", "a", "a", "c"],
+    ["a", "a", "b", "b"],
+    ["a", "a", "b", "c"],
+    ["a", "a", "c", "c"],
+    ["a", "b", "b", "b"],
+    ["a", "b", "b", "c"],
+    ["a", "b", "c", "c"],
+    ["a", "c", "c", "c"],
+    ["b", "b", "b", "b"],
+    ["b", "b", "b", "c"],
+    ["b", "b", "c", "c"],
+    ["b", "c", "c", "c"],
+    ["c", "c", "c", "c"],
+  ];
+  const actual = [...combinationsWithReplacement("abc", 4)];
+  assertEquals(actual, expected);
+});
+
+Deno.test("r = n", () => {
+  const expected = [
+    ["a", "a", "a"],
+    ["a", "a", "b"],
+    ["a", "a", "c"],
+    ["a", "b", "b"],
+    ["a", "b", "c"],
+    ["a", "c", "c"],
+    ["b", "b", "b"],
+    ["b", "b", "c"],
+    ["b", "c", "c"],
+    ["c", "c", "c"],
+  ];
+  const actual = [...combinationsWithReplacement("abc", 3)];
+  assertEquals(actual, expected);
+});
+
+Deno.test("n > r", () => {
+  const expected = [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [1, 1],
+    [1, 2],
+    [2, 2],
+  ];
+  const actual = [...combinationsWithReplacement([0, 1, 2], 2)];
+  assertEquals(actual, expected);
+});
