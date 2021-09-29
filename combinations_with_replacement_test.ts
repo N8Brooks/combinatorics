f6495a1ad4c2779d680e7fabd9b93733fef9fd35
Nathan Brooks <natebrooks004@gmail.com>
2021-09-28 22:43:21 -0700

    add combinationWithReplacement grid tests

--- combinations_with_replacement_test.ts
+++ combinations_with_replacement_test.ts
@@ -3,6 +3,8 @@ import {
   assertThrows,
 } from "https://deno.land/std@0.108.0/testing/asserts.ts";
 import { combinationsWithReplacement } from "./combinations_with_replacement.ts";
+import { product } from "./product.ts";
+import { range } from "./_util.ts";
 
 Deno.test("negative r", () => {
   assertThrows(
@@ -10,6 +12,11 @@ Deno.test("negative r", () => {
     RangeError,
     "r must be non-negative",
   );
+});
+
+Deno.test("n = 0", () => {
+  const actual = [...combinationsWithReplacement("", 1)];
+  assertEquals(actual, []);
 });
 
 Deno.test("r = 0", () => {
@@ -22,11 +29,6 @@ Deno.test("r = n = 0", () => {
   const expected = [[]];
   const actual = [...combinationsWithReplacement("", 0)];
   assertEquals(actual, expected);
-});
-
-Deno.test("n = 0, r > 0", () => {
-  const actual = [...combinationsWithReplacement("", 1)];
-  assertEquals(actual, []);
 });
 
 Deno.test("r > n", () => {
@@ -80,3 +82,29 @@ Deno.test("n > r", () => {
   const actual = [...combinationsWithReplacement([0, 1, 2], 2)];
   assertEquals(actual, expected);
 });
+
+for (let n = 1; n < 6; n++) {
+  const iterable = range(n);
+  for (let r = 1; r < 6; r++) {
+    Deno.test(`combinationsWithReplacement([${iterable}], ${r})`, () => {
+      const actual = [...combinationsWithReplacement(iterable, r)];
+      const expected1 = [...combinationsWithReplacement1(iterable, r)];
+      assertEquals(actual, expected1);
+    });
+  }
+}
+
+/** Equivalent to `combinationsWithReplacement1` for testing. */
+function* combinationsWithReplacement1<T>(
+  iterable: Iterable<T>,
+  r: number,
+): Generator<T[]> {
+  const pool = [...iterable];
+  const n = pool.length;
+  const factors: number[][] = Array(r).fill(range(n));
+  for (const indices of product(...factors)) {
+    if (!indices.some((x, i) => indices[i - 1] > x)) {
+      yield indices.map((i) => pool[i]);
+    }
+  }
+}
