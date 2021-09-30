9b848a5c10fadca135b65f39740c2b107da82d3a
Nathan Brooks <natebrooks004@gmail.com>
2021-09-30 08:51:23 -0700

    file name and coverage changes

--- _util.ts
+++ _util.ts
@@ -1,9 +1,6 @@
 /** Returns an array of length `n` containing elements `0` through `n - 1` */
 export function range(n: number): number[] {
-  if (!Number.isInteger(n)) {
-    throw RangeError("n must be an integer");
-  }
-  const result = Array(Math.max(0, n));
+  const result = Array(n);
   for (let i = 0; i < n; i++) {
     result[i] = i;
   }
@@ -12,9 +9,6 @@ export function range(n: number): number[] {
 
 /** Return `n` factorial as an integer. */
 export function factorial(n: number): number {
-  if (n < 0 || !Number.isInteger(n)) {
-    throw RangeError("n must be a non-negative integer");
-  }
   let result = 1;
   for (let i = 2; i <= n; i++) {
     result *= i;
