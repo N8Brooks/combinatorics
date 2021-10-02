40766ac0a1d63cc6c983ac7df89eaf6aa8f57d60
Nathan Brooks <natebrooks004@gmail.com>
2021-10-02 16:45:52 -0700

    optimize product

--- product.ts
+++ product.ts
@@ -14,9 +14,10 @@ export function* product<T>(
     yield [];
     return;
   }
+  let i, j, index, result;
   const ns = new Uint32Array(n);
-  const result = Array(n);
-  for (let i = 0; i < n; i++) {
+  result = Array(n);
+  for (i = 0; i < n; i++) {
     const pool = pools[i];
     const { length } = pool;
     if (length === 0) {
@@ -26,18 +27,22 @@ export function* product<T>(
     ns[i] = length - 1;
   }
   yield result;
-  const indices = new Uint32Array(n).fill(0);
+  const indices = new Uint32Array(n);
   while (true) {
     loop: {
-      for (let i = n - 1; i >= 0; i--) {
+      for (i = n - 1; i >= 0; i--) {
         if (indices[i] === ns[i]) {
           continue;
         }
-        indices[i] += 1;
-        indices.fill(0, i + 1);
-        const result = Array(n);
-        for (let j = 0; j < n; j++) {
+        result = Array(n);
+        for (j = 0; j < i; j++) {
           result[j] = pools[j][indices[j]];
+        }
+        index = indices[i] += 1;
+        result[i] = pools[i][index];
+        for (j = i + 1; j < n; j++) {
+          indices[j] = 0;
+          result[j] = pools[j][0];
         }
         yield result;
         break loop;
