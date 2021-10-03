ab4cc1b13b73d681d1a5a60d86ebffe1f098dda8
Nathan Brooks <natebrooks004@gmail.com>
2021-10-02 20:45:34 -0700

    prefer const

--- product.ts
+++ product.ts
@@ -14,17 +14,14 @@ export function* product<T>(
   }
   const pools: T[][] = Array(n);
   const ns: Uint32Array = new Uint32Array(n);
-  let i: number, j: number;
-  let length: number, index: number;
-  let pool: T[], result: T[];
-  result = Array(n);
-  for (i = 0; i < k; i++) {
-    pool = Array.from(iterables[i]);
-    length = pool.length;
+  const result: T[] = Array(n);
+  for (let i = 0; i < k; i++) {
+    const pool = Array.from(iterables[i]);
+    const length = pool.length;
     if (length === 0) {
       return;
     }
-    for (j = i; j < n; j += k) {
+    for (let j = i; j < n; j += k) {
       pools[j] = pool;
       ns[j] = length - 1;
       result[j] = pool[0];
@@ -34,17 +31,17 @@ export function* product<T>(
   const indices: Uint32Array = new Uint32Array(n);
   while (true) {
     loop: {
-      for (i = n - 1; i >= 0; i--) {
+      for (let i = n - 1; i >= 0; i--) {
         if (indices[i] === ns[i]) {
           continue;
         }
-        result = Array(n);
-        for (j = 0; j < i; j++) {
+        const result = Array(n);
+        for (let j = 0; j < i; j++) {
           result[j] = pools[j][indices[j]];
         }
-        index = indices[i] += 1;
+        const index = indices[i] += 1;
         result[i] = pools[i][index];
-        for (j = i + 1; j < n; j++) {
+        for (let j = i + 1; j < n; j++) {
           indices[j] = 0;
           result[j] = pools[j][0];
         }
