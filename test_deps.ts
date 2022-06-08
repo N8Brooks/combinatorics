b299987572566532481afd9474f6badc63558c2f
N8Brooks <natebrooks004@gmail.com>
2022-06-08 10:34:32 -0700

    refactor: add safety checks for test utils

--- test_deps.ts
+++ test_deps.ts
@@ -2,6 +2,9 @@ export * from "https://deno.land/std@0.137.0/testing/asserts.ts";
 
 /** Returns an array of length `n` containing elements `0` through `n - 1`. */
 export function range(n: number): number[] {
+  if (!Number.isInteger(n) || n < 0) {
+    throw RangeError("n must be a non-negative integer");
+  }
   const result = Array(n);
   for (let i = 0; i < n; i++) {
     result[i] = i;
@@ -11,6 +14,9 @@ export function range(n: number): number[] {
 
 /** Return `n` factorial as an integer. */
 export function factorial(n: number): number {
+  if (!Number.isInteger(n) || n < 0) {
+    throw RangeError("n must be a non-negative integer");
+  }
   let result = 1;
   for (let i = 2; i <= n; i++) {
     result *= i;
